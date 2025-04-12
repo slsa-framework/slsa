b1d45a73a09a97a386d00ae1e44c7fa025c0e683
Zachariah Cox <zachariahcox@gmail.com>
2025-04-12 16:01:00 -0400

    add a reset. should clean up the spec folders but not delete the build folder
    Signed-off-by: Zachariah Cox <zachariahcox@gmail.com>

--- combine-versions.sh
+++ combine-versions.sh
@@ -2,24 +2,29 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 mkdir build
 cd build
 
+git reset --hard
 git checkout users/zc/refactor-main
 mkdir draft 
 cp -r ../docs/spec/* draft/
 
 version=v1.1-rc1
+git reset --hard
 git checkout releases/$version
 mkdir $version
 cp -r ../docs/spec/* $version/
 
 version=v1.1-rc2
+git reset --hard
 git checkout releases/$version
 mkdir $version
 cp -r ../docs/spec/* $version/
 
 version=v1.0
+git reset --hard
 git checkout releases/$version
 mkdir $version
 cp -r ../docs/spec/* $version/
 
 # back to the original branch
+git reset --hard
 git checkout $CURRENT_BRANCH
