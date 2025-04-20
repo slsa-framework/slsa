6806635c6847ad55a22d2b8eb4bff2a374a9e24d
Zachariah Cox <zachariahcox@gmail.com>
2025-04-20 14:03:06 +0000

    consolidate tools, disable workflow files

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
+# Exit immediately if a command exits with a non-zero status
 set -e
 
+# Navigate to the 'www' directory
 cd www
+
+# Configure Bundler to install gems locally in the 'vendor/bundle' directory
 bundle config set --local path 'vendor/bundle'
+
+# Install Ruby gems specified in the Gemfile
 bundle install
+
+# Install the Netlify CLI globally using npm
 npm install -g netlify-cli
+
+# Return to the previous directory
 cd ..
