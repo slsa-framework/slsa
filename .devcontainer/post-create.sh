ad43814cb99ab960ccaeab51871fa513d7ffcff1
Arnaud J Le Hors <lehors@us.ibm.com>
2026-02-16 12:13:03 +0100

    impl: Fix DevContainer on ARM (#1573)
    Netlify 23.15.1 on ARM fails to install a working version of deno, so
    launching netlify then fails. This installs the latest version of deno
    which works on ARM.
    
    Also expand a bit the documentation on using the DevContainer.
    
    FYI, here is the error (which I did report) on running `netlify dev -c
    'bundle exec jekyll serve --livereload --incremental'`:
    ```
    ✖ Setting up the Edge Functions environment. This may take a couple of minutes.
     ›   Error: Netlify CLI has terminated unexpectedly
    This is a problem with the Netlify CLI, not with your application.
    If you recently updated the CLI, consider reverting to an older version by running:
    
    npm install -g netlify-cli@VERSION
    
    You can use any version from https://ntl.fyi/cli-versions.
    
    Please report this problem at https://ntl.fyi/cli-error including the error details below.
    
    Error: Failed to set up Deno for Edge Functions.
    Error: Command failed with exit code 255: /root/.config/netlify/deno-cli/deno --version
    qemu-x86_64-static: Could not open '/lib64/ld-linux-x86-64.so.2': No such file or directory
    Downloaded to: /root/.config/netlify/deno-cli/deno
    Platform: linux/arm64
    
    ```
    
    ---------
    
    Signed-off-by: Arnaud J Le Hors <lehors@us.ibm.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -14,5 +14,10 @@ bundle install
 # Install the Netlify CLI globally using npm
 npm install -g netlify-cli
 
+# Netlify 23.15.1 on ARM fails to install a working version of deno,
+# so install the latest one directly.
+# Note: this should not be necessary once netlify is fixed
+npm install -g deno
+
 # Return to the previous directory
 cd ..
